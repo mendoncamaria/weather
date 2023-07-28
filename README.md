86bd5675b2f2284756be756c99bba3fda8460242
Maria Mendonca <maria.mendonca156@gmail.com>
2023-07-28 13:42:14 +0530

    Code commit, integrated functionality

--- README.md
+++ README.md
@@ -9,3 +9,19 @@ Currently, two official plugins are available:
 
 
 'npm run dev'
+
+```
+<a href="https://www.freepik.com/free-photo/cloud-blue-sky_1017702.htm#query=sunny%20weather&position=0&from_view=keyword&track=ais">Image by jannoon028</a> on Freepik
+
+<a href="https://www.freepik.com/free-photo/white-cloud-blue-sky_3707296.htm#query=sunny%20weather&position=1&from_view=keyword&track=ais#position=1&query=sunny%20weather"> Image by lifeforstock </a>
+
+<a href="https://www.freepik.com/free-vector/realistic-clouds-with-falling-rain_18309129.htm#query=rainy%20weather&position=15&from_view=search&track=ais">Image by starline</a> on Freepik
+
+<a href="https://www.freepik.com/free-photo/view-snowy-mountain-fir-trees-with-blue-sky-background_20110211.htm#query=winter%20weather&position=10&from_view=search&track=ais">Image by h9images</a> on Freepik
+
+<a href="https://www.freepik.com/free-photo/winter-landscape-mountain-village-ukrainian-carpathians-vibrant-night-sky-with-stars-nebula-galaxy-deep-sky-astrophoto_9143817.htm#query=winter%20weather%20night&position=6&from_view=search&track=ais">Image by standret</a> on Freepik
+
+<a href="https://www.freepik.com/free-photo/amazing-beautiful-sky-with-clouds_10472846.htm#query=rain%20weather%20night&position=1&from_view=search&track=ais">Image by user3802032</a> on Freepik
+
+Image by <a href="https://www.freepik.com/free-photo/low-angle-nature-night-time_5481670.htm#query=clear%20weather%20night&position=15&from_view=search&track=ais">Freepik</a>
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React, { useState } from 'react'
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=84a35bade62155fdbf841a131dd35c73`;
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation('');
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name !== undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{data.main.feels_like.toFixed()}°F</p>
+              ) : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? (
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+              ) : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
