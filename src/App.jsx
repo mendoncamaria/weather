b244acb3f7bc92f40c1a087b25227ac4cb407929
Maria Mendonca <maria.mendonca156@gmail.com>
2024-08-18 08:57:58 +0530

    moved primary code to component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import axios from 'axios';
 import { KEYS } from './keys/secret';
+import Main from './components/main';
 
 function App() {
   const [data, setData] = useState({});
@@ -11,10 +12,13 @@ function App() {
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
-      axios.get(url).then((response) => {
-        setData(response.data);
-        console.log(response.data);
-      });
+      // axios
+      //   .get(url)
+      //   .then((response) => {
+      //     setData(response.data);
+      //     console.log(response.data);
+      //   })
+      //   .catch((error) => console.log(error));
       setLocation('');
     }
   };
@@ -31,42 +35,7 @@ function App() {
             type="text"
           />
         </div>
-        <div className="container">
-          <div className="top">
-            <div className="location">
-              <p>{data.name}</p>
-            </div>
-            <div className="temp">
-              {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
-            </div>
-            <div className="description">
-              {data.weather ? <p>{data.weather[0].main}</p> : null}
-            </div>
-          </div>
-
-          {data.name !== undefined && (
-            <div className="bottom">
-              <div className="feels">
-                {data.main ? (
-                  <p className="bold">{data.main.feels_like.toFixed()}°F</p>
-                ) : null}
-                <p>Feels Like</p>
-              </div>
-              <div className="humidity">
-                {data.main ? (
-                  <p className="bold">{data.main.humidity}%</p>
-                ) : null}
-                <p>Humidity</p>
-              </div>
-              <div className="wind">
-                {data.wind ? (
-                  <p className="bold">{data.wind.speed.toFixed()} MPH</p>
-                ) : null}
-                <p>Wind Speed</p>
-              </div>
-            </div>
-          )}
-        </div>
+        <Main data={data} />
       </div>
       <footer
         style={{
