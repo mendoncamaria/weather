33488fcbffb1db19d89b4de39ed501411ad0893c
Maria Mendonca <maria.mendonca156@gmail.com>
2024-08-24 20:27:25 +0530

    Updated Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,3 @@
-// import React from 'react'
-
 import { useState, useEffect } from 'react';
 import { getTime, getToday } from '../helper/utils';
 
@@ -15,10 +13,72 @@ function Header() {
     return () => clearInterval(intervalId);
   }, []);
 
+  const [temperatureUnit, setTemperatureUnit] = useState('degC');
+
+  const handleUnitChange = (unit) => {
+    setTemperatureUnit(unit);
+  };
+
   return (
-    <div>
-      <p>{time}</p>
-      <p>{date}</p>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        padding: '1.5rem',
+        alignItems: 'center',
+      }}
+    >
+      <div>
+        <p
+          style={{
+            fontSize: '2rem',
+          }}
+        >
+          {time}
+        </p>
+        <p>{date}</p>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          gap: '5px',
+        }}
+      >
+        <button
+          style={{
+            width: '40px',
+            height: '40px',
+            borderRadius: '50%',
+            padding: '0',
+            backgroundColor:
+              temperatureUnit === 'degC' ? '#007bff' : 'transparent',
+            color: temperatureUnit === 'degC' ? '#fff' : '#000',
+            border: '1px solid',
+            borderColor: temperatureUnit === 'degC' ? '#007bff' : '#d1c9c9',
+            cursor: 'pointer',
+          }}
+          onClick={() => handleUnitChange('degC')}
+        >
+          °C
+        </button>
+        <button
+          style={{
+            width: '40px',
+            height: '40px',
+            borderRadius: '50%',
+            padding: '0',
+            backgroundColor:
+              temperatureUnit === 'degF' ? '#007bff' : 'transparent',
+            color: temperatureUnit === 'degF' ? '#fff' : '#000',
+            border: '1px solid',
+            borderColor: temperatureUnit === 'degF' ? '#007bff' : '#d1c9c9',
+            cursor: 'pointer',
+          }}
+          onClick={() => handleUnitChange('degF')}
+        >
+          °F
+        </button>
+      </div>
     </div>
   );
 }
