63c6dd708f16af4a4d6006fc4b201778b566058b
Maria Mendonca <maria.mendonca156@gmail.com>
2024-08-24 20:27:40 +0530

    use header in dash

--- src/container/Dashboard.jsx
+++ src/container/Dashboard.jsx
@@ -1,9 +1,13 @@
+import Header from "../components/Header";
+
 function Dashboard() {
   return (
     <div style={{ display: 'flex' }}>
       <div
         style={{ backgroundColor: '#f1f1f1', width: '70vw', height: '100vh' }}
-      ></div>
+      >
+        <Header />
+      </div>
       <div
         style={{
           backgroundImage: 'url(src/assets/clouds.jpeg)',
