e05dd278f3a7d4cb8da5e8783c8e5986cb85e78a
Maria Mendonca <maria.mendonca156@gmail.com>
2024-08-27 10:14:09 +0530

    Added week forecast array

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,3 +11,55 @@ export const IMAGES = [
   { src: NewYork, alt: 'New York', lat: '40.7127281', lon: '-74.0060152' },
   { src: Pretoria, alt: 'Pretoria', lat: '-25.7459277', lon: '28.1879101' },
 ];
+
+export const WEEK_FORECAST = [
+  {
+    day: 'Monday',
+    status: 'Rainy',
+    wind: '11.2kmph',
+    humidity: '23%',
+    temperature: '26deg',
+  },
+  {
+    day: 'Tuesday',
+    status: 'Rainy',
+    wind: '11.2kmph',
+    humidity: '23%',
+    temperature: '26deg',
+  },
+  {
+    day: 'Wednesday',
+    status: 'Rainy',
+    wind: '11.2kmph',
+    humidity: '23%',
+    temperature: '26deg',
+  },
+  {
+    day: 'Thursday',
+    status: 'Rainy',
+    wind: '11.2kmph',
+    humidity: '23%',
+    temperature: '26deg',
+  },
+  {
+    day: 'Friday',
+    status: 'Rainy',
+    wind: '11.2kmph',
+    humidity: '23%',
+    temperature: '26deg',
+  },
+  {
+    day: 'Saturday',
+    status: 'Rainy',
+    wind: '11.2kmph',
+    humidity: '23%',
+    temperature: '26deg',
+  },
+  {
+    day: 'Sunday',
+    status: 'Rainy',
+    wind: '11.2kmph',
+    humidity: '23%',
+    temperature: '26deg',
+  },
+];
