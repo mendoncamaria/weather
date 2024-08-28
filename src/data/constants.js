602db4dab33997600e927c2e7ac2acfa5f0ce220
Maria Mendonca <maria.mendonca156@gmail.com>
2024-08-28 08:02:19 +0530

    added 5 cities for instant forecast

--- src/data/constants.js
+++ src/data/constants.js
@@ -3,6 +3,11 @@ import Dubai from '../assets/Dubai.jpeg';
 import Melbourne from '../assets/Mebourne.jpeg';
 import NewYork from '../assets/NewYork.jpeg';
 import Pretoria from '../assets/Pretoria.jpeg';
+import Cairo from '../assets/Cairo.jpeg';
+import RioDeJanerio from '../assets/RioDeJanerio.jpeg';
+import Paris from '../assets/Paris.jpeg';
+import HongKong from '../assets/HongKong.jpeg';
+import Barcelona from '../assets/Barcelona.jpeg';
 
 export const IMAGES = [
   { src: London, alt: 'London', lat: '51.507351', lon: '-0.127758' },
@@ -10,6 +15,16 @@ export const IMAGES = [
   { src: Melbourne, alt: 'Melbourne', lat: '-37.813629', lon: '144.963058' },
   { src: NewYork, alt: 'New York', lat: '40.7127281', lon: '-74.0060152' },
   { src: Pretoria, alt: 'Pretoria', lat: '-25.7459277', lon: '28.1879101' },
+  { src: Cairo, alt: 'Cairo', lat: '30.044420', lon: '31.235712' },
+  {
+    src: RioDeJanerio,
+    alt: 'Rio De Janerio',
+    lat: '-22.906847',
+    lon: '-43.172897',
+  },
+  { src: Paris, alt: 'Paris', lat: '48.856613', lon: '2.352222' },
+  { src: HongKong, alt: 'Hong Kong', lat: '22.396427', lon: '114.109497' },
+  { src: Barcelona, alt: 'Barcelona', lat: '41.385063', lon: '2.173404' },
 ];
 
 export const WEEK_FORECAST = [
